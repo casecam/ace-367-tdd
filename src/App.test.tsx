b34d3e1ac2c332aefc3cb893c8db1a2ffb5539a8
caseycameron <mcasey.cameron@gmail.com>
2022-07-25 15:31:29 -0700

    RED: renders the paragraph

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,11 @@ import { render, screen } from '@testing-library/react';
 import React from 'react';
 
 import App from './App';
-test('renders the heading', () => {
+it('renders the heading', () => {
   render(<App />);
   screen.getByRole('heading', {name: /hello react/i})
 });
+it('renders the paragraph', () => {
+  render(<App />);
+  screen.getByText(/nice tdd/i)
+})
