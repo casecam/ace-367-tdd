18bb10983cd0f2625e49b5d7e95ab85d532ba7aa
caseycameron <mcasey.cameron@gmail.com>
2022-07-25 15:40:16 -0700

    refactor to class component (green tests)

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,6 +2,7 @@ import { render, screen } from '@testing-library/react';
 import React from 'react';
 
 import App from './App';
+
 it('renders the heading', () => {
   render(<App />);
   screen.getByRole('heading', {name: /hello react/i})
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div>
-     <h1>Hello React</h1>
-     <p>Nice TDD</p>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello React</h1>
+        <p>Nice TDD</p>
+      </div>
+    )
+  }
 }
 
-export default App;
+
+// import React from 'react';
+
+// function App() {
+//   function label() {
+//     return 'Hello React'
+//   }
+//   return (
+//     <div>
+//      <h1>{label()}</h1>
+//      <p>Nice TDD</p>
+//     </div>
+//   );
+// }
+
+// export default App;
