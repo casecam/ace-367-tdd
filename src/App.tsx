ee4e5694e0847f0bbde33846b26950a090b42ad3
caseycameron <mcasey.cameron@gmail.com>
2022-07-25 16:29:12 -0700

    onClick added to h1

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <h1>{this.label}</h1>
+        <h1 onClick={() => alert('Hello World')}>{this.label}</h1>
         <p>Nice TDD</p>
       </div>
     )
