2f7d8f508f4ccae196b8590ab5dd52f229e31b8b
caseycameron <mcasey.cameron@gmail.com>
2022-07-25 16:49:00 -0700

    Red - refactor Heading for props and TS

--- src/Heading.test.tsx
+++ src/Heading.test.tsx
@@ -2,6 +2,6 @@ import { render, screen } from '@testing-library/react';
 import Heading from './Heading';
 
 it('renders the heading', () => {
-  render(<Heading />);
+  render(<Heading recipient={'World'}/>);
   screen.getByRole('heading', { name: /hello react/i})
 })
--- src/Heading.tsx
+++ src/Heading.tsx
@@ -1,3 +1,7 @@
 import * as React from 'react'
-const Heading: React.FC = () => <h1>Hello React</h1>
+interface IHeadingProps {
+  recipient: string
+}
+
+const Heading: React.FC<IHeadingProps> = (props) => <h1>Hello {props.recipient}</h1>
 export default Heading
