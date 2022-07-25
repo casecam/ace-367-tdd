f31492ea7203bc17d8130e36c6801814a5070646
caseycameron <mcasey.cameron@gmail.com>
2022-07-25 16:30:08 -0700

    onClick refactor

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,13 @@ import React, { Component } from 'react'
 export default class App extends Component {
   public label = 'Hello react'
 
+  public handleClick () {
+    alert('Hello World')
+  }
   render() {
     return (
       <div>
-        <h1 onClick={() => alert('Hello World')}>{this.label}</h1>
+        <h1 onClick={this.handleClick}>{this.label}</h1>
         <p>Nice TDD</p>
       </div>
     )
