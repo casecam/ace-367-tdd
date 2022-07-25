16d6f20a76e85375e9ebc5515eddaa7b7d5badcb
caseycameron <mcasey.cameron@gmail.com>
2022-07-25 14:52:49 -0700

    RED: basic test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
+import { render } from '@testing-library/react';
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
 
-test('renders learn react link', () => {
+import App from './App';
+test('renders without crashing', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(1).toBe(2)
+  
 });
