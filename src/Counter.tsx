4b2f2b8240657840fd1432244f1917af56e0595c
caseycameron <mcasey.cameron@gmail.com>
2022-07-26 12:18:31 -0700

    Refactor - Counter

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,41 +1,16 @@
 import React, { Component } from 'react'
 
-interface ICounterProps {
-  label?: string
-  start?: number
-}
-
-interface ICounterState {
-  count: number
-}
-export default class Counter extends Component<ICounterProps, ICounterState> {
-  public static defaultProps = {
-    label: 'Count',
-    start: 0
-  }
-
-  constructor(props: ICounterProps) {
-    super(props)
-    this.state = {
-      count: props.start!
-    }
-  }
-
-  public handleClick = (event: React.MouseEvent<HTMLElement>) => {
-    const inc: number = event.shiftKey ? 10 : 1
-    this.setState({count: this.state.count + inc})
-  }
-
-  render() {
+const Counter: React.FC<ICounterProps> = (
+  {label = 'Count', count}
+) => {
     return (
       <div
         data-testid="counter"
         className="counter"
-        onClick={this.handleClick}
+        // onClick={handleClick}
       >
-        <label>{this.props.label}</label>
-        <span>{this.state.count}</span>
+        <label>{label}</label>
+        <span>{count}</span>
       </div>
     )
-  }
 }
