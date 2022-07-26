70b2d3a9f0bd9a5fcac9eb6e94c3a635eed4539c
caseycameron <mcasey.cameron@gmail.com>
2022-07-26 10:43:44 -0700

    Green - passing label with props test

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,10 +1,17 @@
 import React, { Component } from 'react'
 
-export default class Counter extends Component {
+interface ICounterProps {
+  label?: string
+}
+export default class Counter extends Component<ICounterProps> {
+  public static defaultProps = {
+    label: 'Count'
+  }
+
   render() {
     return (
       <div className="counter">
-        <label>Count</label>
+        <label>{this.props.label}</label>
         <span>1</span>
       </div>
     )
