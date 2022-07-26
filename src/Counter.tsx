6bc219e1ca950b8376d0e30d095c6da1ed175dfd
caseycameron <mcasey.cameron@gmail.com>
2022-07-26 10:56:12 -0700

    Green - state for Counter, defaults to zero

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -3,16 +3,27 @@ import React, { Component } from 'react'
 interface ICounterProps {
   label?: string
 }
-export default class Counter extends Component<ICounterProps> {
+
+interface ICounterState {
+  count: number
+}
+export default class Counter extends Component<ICounterProps, ICounterState> {
   public static defaultProps = {
     label: 'Count'
+  }
+
+  constructor(props: ICounterProps) {
+    super(props)
+    this.state = {
+      count: 0
+    }
   }
 
   render() {
     return (
       <div className="counter">
         <label>{this.props.label}</label>
-        <span>1</span>
+        <span>{this.state.count}</span>
       </div>
     )
   }
