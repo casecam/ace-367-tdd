5dd935797fc41dd759e450bff7d77b3b67cc5e0f
caseycameron <mcasey.cameron@gmail.com>
2022-07-26 14:19:53 -0700

    Heading - FC refactor

--- src/Heading.tsx
+++ src/Heading.tsx
@@ -1,8 +1,7 @@
 import * as React from 'react'
 interface IHeadingProps {
-  // ? needed for recipient's default prop value in the component
   recipient?: string
 }
 
-const Heading: React.FC<IHeadingProps> = ({ recipient = 'React'}) => <h1>Hello {recipient}</h1>
+const Heading = ({ recipient = 'React'}: IHeadingProps) => <h1>Hello {recipient}</h1>
 export default Heading
